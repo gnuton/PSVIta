6daa237299e1855ddd5a9f864adad387eb565c86
Antonio Aloisio <gnuton@gnuton.org>
2018-08-02 08:06:58 +0300

    Wrapped DebugScreen code in a CPP class. More to come

--- hello_cpp_world/src/main.cpp
+++ hello_cpp_world/src/main.cpp
@@ -1,8 +1,21 @@
 #include <psp2/kernel/processmgr.h>
 #include <sstream>
 #include <vector>
-
+#include <string>
 #include <cstdio>
+#include "debugScreen.h"
+
+
+class DebugScreen {
+public:
+    DebugScreen() {
+        psvDebugScreenInit();
+    }
+
+    void printf(const std::string& msg) {
+        psvDebugScreenPrintf("%s\n", msg.c_str());
+    }
+};
 
 int main(int argc, char *argv[]) {
 	std::stringstream output;
@@ -14,8 +27,12 @@ int main(int argc, char *argv[]) {
 		// std::cout does't work ATM :(
 		output << s;
 	}
+    auto ds = new DebugScreen();
 	output << std::endl;
-	printf("%s\n", output.str().c_str());
+    ds->printf(output.str());
+    ds->printf("\e[10;20H TEST");
+
+    sceKernelDelayThread(20*1000*1000);
 	sceKernelExitProcess(0);
     return 0;
 }
